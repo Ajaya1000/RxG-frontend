b2b63dc2b4691d75d81d7a6cf14b19d4965db377
Ajaya1000 <ajaymati1000@gmail.com>
2021-05-31 22:12:02 +0530

    fixed linter issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'no-unused-vars': 1,
+        'react/prop-types': 0,
+        'react/jsx-props-no-spreading': 0,
+    },
 };
