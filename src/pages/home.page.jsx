e1bf1d5d130c9b2370e75b75832abb3ce13749ce
Ajaya1000 <ajaymati1000@gmail.com>
2021-06-01 13:04:16 +0530

    routeand pages setup

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+    const [userName, setUserName] = useState('');
+    const [existingRoomCode, setExistingRoomCode] = useState('');
+    const [newRoomCode, setNewRoomCode] = useState('');
+    const [level, setLevel] = useState(0);
+};
+
+export default Home;
