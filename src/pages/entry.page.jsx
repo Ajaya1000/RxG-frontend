ee07e41247d962bea30ab3973380e28a65b653ec
Ajaya1000 <ajaymati1000@gmail.com>
2021-06-03 12:14:47 +0530

    change endpoint to heroku

--- src/pages/entry.page.jsx
+++ src/pages/entry.page.jsx
@@ -6,7 +6,7 @@ import Alert from '@material-ui/lab/Alert';
 import Game from '../component/game.component';
 import Lobby from '../component/lobby.component';
 
-const ENDPOINT = 'http://127.0.0.1:4000';
+const ENDPOINT = 'https://serene-plateau-46910.herokuapp.com/';
 
 const useQuery = () => new URLSearchParams(useLocation().search);
 
